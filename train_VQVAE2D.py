355f0898bb14bb901537de09f165622edb3130a5
Chris Tava <christava@microsoft.com>
2024-09-14 20:02:16 -0400

    chg: batch size

--- train_VQVAE2D.py
+++ train_VQVAE2D.py
@@ -38,7 +38,7 @@ def get_parser():
     parser.add_argument('--gradient_clip_val', default=1.0)
     # lightning args
     parser.add_argument("--max_epochs", type=int, default=1)
-    parser.add_argument("--limit_train_batches", type=int, default=10)
+    parser.add_argument("--limit_train_batches", type=int, default=1000)
     parser.add_argument('--profiler', default='simple')
     parser.add_argument('--accelerator', default='gpu')
     parser.add_argument('--precision', default='32')
