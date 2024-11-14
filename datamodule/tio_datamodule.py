d397b20daf0d35c5e0d11743627c05c022a7bdce
Chris Tava <christava@microsoft.com>
2024-11-14 09:46:19 -0500

    chg: tio_datamodule - image_reader

--- datamodule/tio_datamodule.py
+++ datamodule/tio_datamodule.py
@@ -11,7 +11,7 @@ import json
 import SimpleITK as sitk
 import nibabel as nib
 
-def image_reader(path):
+def image_reader_old(path):
     data = np.load(path, allow_pickle=True)
     data = torch.from_numpy(data).float()
     data /= 255.
@@ -19,6 +19,11 @@ def image_reader(path):
     affine = np.eye(4)
     return data, affine
 
+def image_reader(path):
+    data = np.load(path, allow_pickle=True)
+    data = data.astype(np.float32)
+    sitk_image = sitk.GetImageFromArray(data)
+    return sitk_image
 
 def label_reader(path):
     data = np.load(path, allow_pickle=True)
