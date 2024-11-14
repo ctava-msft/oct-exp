af880c243e736899be9f706387f268dca4203863
Chris Tava <christava@microsoft.com>
2024-11-13 22:45:29 -0500

    chg: save_as_npy - single directory

--- utils/save_as_npy.py
+++ utils/save_as_npy.py
@@ -1,15 +1,15 @@
-import argparse
+import os
 import cv2 as cv
 import numpy as np
 from natsort import natsorted
-import os
+import argparse
 
 def read_img_to_np(img_dir, cvflag=cv.IMREAD_GRAYSCALE):
     assert os.path.exists(img_dir), f"Directory does not exist: {img_dir}"
     print(f"Reading images from directory: {img_dir}")
     imgs = []
     names = natsorted([name for name in os.listdir(img_dir) if name.endswith('.bmp')])
-    #print(f"Found images: {names}")
+    print(f"Found .bmp images: {names}")
     for name in names:
         img_path = os.path.join(img_dir, name)
         img = cv.imread(img_path, cvflag)
@@ -19,27 +19,27 @@ def read_img_to_np(img_dir, cvflag=cv.IMREAD_GRAYSCALE):
     imgs = np.stack(imgs, axis=0)
     return imgs
 
-def save(src_dir, dst_dir):
-    os.makedirs(dst_dir, exist_ok=True)
+def save(src_dir):
     names = []
     for root, dirs, files in os.walk(src_dir):
         for file in files:
-            names.append(os.path.join(root, file))
+            if file.endswith('.bmp'):
+                names.append(os.path.join(root, file))
     names = natsorted(names)
     for i, name in enumerate(names):
         print(f"Processing file {i}: {name}")
         oct_data = read_img_to_np(os.path.dirname(name), cv.IMREAD_GRAYSCALE)
         base_name = os.path.splitext(os.path.basename(name))[0]  # Remove the extension
-        np.save(os.path.join(dst_dir, base_name + '.npy'), oct_data)
-        print(f"Saved {base_name}.npy to {dst_dir}")
+        npy_path = os.path.join(os.path.dirname(name), base_name + '.npy')
+        np.save(npy_path, oct_data)
+        print(f"Saved {base_name}.npy to {os.path.dirname(name)}")
 
 def main():
-    parser = argparse.ArgumentParser(description="Convert images to numpy files.")
-    parser.add_argument('src_dir', type=str, help="Source directory containing images.")
-    parser.add_argument('dst_dir', type=str, help="Destination directory to save numpy files.")
+    parser = argparse.ArgumentParser(description="Convert .bmp images to numpy files.")
+    parser.add_argument('src_dir', type=str, help="Source directory containing .bmp images.")
     args = parser.parse_args()
     
-    save(args.src_dir, args.dst_dir)
+    save(args.src_dir)
 
 if __name__ == "__main__":
     main()
