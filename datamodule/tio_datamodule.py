317d703635b6f904123524fe1a58f8a9f074a990
Chris Tava <christava@microsoft.com>
2024-11-14 22:01:47 -0500

    chg: datamodule - num_workers

--- datamodule/tio_datamodule.py
+++ datamodule/tio_datamodule.py
@@ -146,10 +146,10 @@ class TioDatamodule(pl.LightningDataModule):
         self.test_set = tio.SubjectsDataset(self.test_subjects, transform=preprocess)
 
     def train_dataloader(self):
-        return SubjectsLoader(self.patch_train_set, batch_size=self.batch_size)
+        return SubjectsLoader(self.patch_train_set, batch_size=self.batch_size, num_workers=self.num_workers)
 
     def val_dataloader(self):
-        return SubjectsLoader(self.test_set, batch_size=self.batch_size, shuffle=False)
+        return SubjectsLoader(self.test_set, batch_size=self.batch_size, shuffle=False, num_workers=self.num_workers)
     
     def test_dataloader(self):
-        return SubjectsLoader(self.test_set, batch_size=self.batch_size, shuffle=False, num_workers=4)
+        return SubjectsLoader(self.test_set, batch_size=self.batch_size, shuffle=False, num_workers=self.num_workers)
