0ebbd0a9619f8945205d04b197998c9c9435200d
Chris Tava <christava@microsoft.com>
2024-11-14 19:18:17 -0500

    chg: tio data module - subjects loader again-2

--- datamodule/tio_datamodule.py
+++ datamodule/tio_datamodule.py
@@ -1,5 +1,5 @@
 # -*- coding:utf-8 -*-
-import torchio.SubjectsLoader as SubjectsLoader
+import torchio.data.SubjectsLoader as SubjectsLoader
 import pytorch_lightning as pl
 import os
 import torchio as tio
