ca6d644d945c87f2d9bea346dbea2568c8830c75
Chris Tava <christava@microsoft.com>
2024-11-17 23:15:38 -0500

    chg: ldm2d-tensor-2

--- datamodule/latent_refiner_datamodule.py
+++ datamodule/latent_refiner_datamodule.py
@@ -58,7 +58,7 @@ class latent_Dataset(Dataset):
 
     def __getitem__(self, index):
 
-        latent_1 = np.load(self.latent_1_paths[index])
+        latent_1 = np.load(self.latent_1_paths[index], allow_pickle=True)
         latent_1 = torch.from_numpy(latent_1).float()[:, :, :]
 
         latent_2 = np.load(self.latent_2_paths[index])
