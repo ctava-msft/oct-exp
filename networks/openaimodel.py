6e901f26ff5c869ada1b80ce895d7e1cd5f5a5c9
Chris Tava <christava@microsoft.com>
2024-11-18 07:59:48 -0500

    chg: ldm2d-tensor-26

--- networks/openaimodel.py
+++ networks/openaimodel.py
@@ -152,7 +152,8 @@ class TimestepEmbedSequential(nn.Sequential, TimestepBlock):
                 total_elements = x.numel()
 
                 # Define the target shape
-                new_shape = (total_elements // (11 * 640 * 400), 11, 640, 400)
+                #new_shape = (total_elements // (11 * 640 * 400), 11, 640, 400)
+                new_shape = (32, 10, 640, 400)
                 print(f"Original shape: {x.shape}")
                 print(f"Total elements: {total_elements}")
                 print(f"New shape: {new_shape}")
