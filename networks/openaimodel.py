2aa46a081ba5c004868273c2edaeed81a0141558
Chris Tava <christava@microsoft.com>
2024-11-17 22:23:19 -0500

    chg: kernel_size-4

--- networks/openaimodel.py
+++ networks/openaimodel.py
@@ -329,10 +329,10 @@ class ResBlock(TimestepBlock):
 
     def _forward(self, x, emb):
         kernel_size = (min(2, x.shape[2]), min(2, x.shape[3]), min(2, x.shape[4]))
-        print(f"Input shape: {x.shape} kernel_size: {kernel_size}")
+        print(f"Input shape: {x.shape}, kernel_size: {kernel_size}")
         kernel_depth, kernel_height, kernel_width = kernel_size
-        if x.shape[-1] < kernel_width or x.shape[-2] < kernel_height or x.shape[-3] < kernel_depth:
-            raise ValueError("Input dimensions are smaller than the kernel size.")
+        if x.shape[-1] <= kernel_width or x.shape[-2] <= kernel_height or x.shape[-3] <= kernel_depth:
+            raise ValueError(f"Input dimensions {x.shape[-3:]}, are smaller than or equal to the kernel size {kernel_size}.")
         if self.updown:
             in_rest, in_conv = self.in_layers[:-1], self.in_layers[-1]
             h = in_rest(x)
