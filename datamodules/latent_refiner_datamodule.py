d72ae69ad020bcfa1e66bb73a4ae97c84bead7d3
Chris Tava <christava@microsoft.com>
2024-11-19 12:39:09 -0500

    chg: create and use volumes-4

--- datamodules/latent_refiner_datamodule.py
+++ datamodules/latent_refiner_datamodule.py
@@ -58,10 +58,10 @@ class latent_Dataset(Dataset):
     def __getitem__(self, index):
 
         latent_1 = np.load(self.latent_1_paths[index], allow_pickle=True)
-        latent_1 = torch.from_numpy(latent_1).float()[0,:,:,:]
+        latent_1 = torch.from_numpy(latent_1).float()
 
         latent_2 = np.load(self.latent_2_paths[index])
-        latent_2 = torch.from_numpy(latent_2).float()[0,:,:,:]
+        latent_2 = torch.from_numpy(latent_2).float()
         return {'latent_1':latent_1,'latent_2':latent_2,
                 'latent_1_path': self.latent_1_paths[index],
                 'latent_2_path': self.latent_2_paths[index]}
